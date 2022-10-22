77f6c86981f6f23eaa6d253e879097573a58e8c0
Daniel <danielmontrest@gmail.com>
2022-10-21 19:39:51 -0500

    mensaje

--- proyecto1.py
+++ proyecto1.py
@@ -1,1 +1,22 @@
 # Curso de python
+
+# Cambiando tipo de datos 
+ 
+
+numero1 = "5"
+numero2 = "7"
+
+suma = numero1 + numero2
+
+
+# la suma será una adecion de caracteres ya que no son numeros
+print (suma)
+
+# se cambia tipo de dato por numeros para obtener una suma aritmetica
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+suma = numero1 + numero2
+
+print (suma)
