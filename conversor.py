fc42bd526a42f4c162a2b562879d9e7b3ea17156
Daniel <danielmontrest@gmail.com>
2022-10-21 20:40:07 -0500

    mensaje

---
+++ conversor.py
@@ -0,0 +1,11 @@
+
+
+
+
+pesos = input ("¿Cuantos pesos colombianos tienes?: ")
+pesos = float(pesos)
+valor_dolar = 4000
+dolares = pesos / valor_dolar 
+dolares = str(dolares)
+
+print ("Tienes $" + dolares + " dólares")
