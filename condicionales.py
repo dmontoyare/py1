5e06e6b6ef2782936a5c4e6e82c09d05122e674b
Daniel <danielmontrest@gmail.com>
2022-10-21 21:52:51 -0500

    Mensaje

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+
+
+# Este programa permite identificar si el usuario es mayor de edad o no
+
+
+print("Bienvenido por favor siga los pasos para identificar si uted es mayor de edad")
+
+edad = int(input("Por favor escribe tu edad: "))
+
+if edad > 17:
+    print("Eres Mayor de edad")
+else:
+    print("Eres menor de edad")
